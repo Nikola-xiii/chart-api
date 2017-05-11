f4d11ff2b3190ff5d12cce5aa0e35dbd3c071466
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2017-05-11 09:43:37 +0300

    Rewrite funnel widget use jQuery

--- charts/funnel.js
+++ charts/funnel.js
@@ -30,43 +30,43 @@
       var wrapperEl;
 
       if (!wrapper) {
-        wrapperEl = document.createElement('div');
+        wrapperEl = $('div');
         console.warn('Container not defined.');
       } else if (typeof wrapper === 'string') {
-        wrapperEl = document.querySelector(wrapper);
+        wrapperEl = $(wrapper);
       } else if (wrapper.nodeName) {
         wrapperEl = wrapper;
       }
 
-      wrapperEl.classList.add('wrapper-funnel-bar');
+      //wrapperEl.classList.add('wrapper-funnel-bar');
 
       if (this.config.id) {
         wrapperEl.setAttribute('id', this.config.id);
       }
 
-      this.width = wrapperEl.offsetWidth / 2 - 10;
+      this.width = wrapperEl[0].offsetWidth / 2 - 10;
 
       return wrapperEl;
     },
 
     _getContainer: function() {
-      var container = document.createElement('div');
-      container.setAttribute('class', 'funnel-bar');
+      var container = $('div');
+      container.attr('class', 'funnel-bar');
       container.append(this._getChartContainer());
-      container.append(this._getTableContainer());
+      //container.append(this._getTableContainer());
       return container;
     },
 
-    _getTableContainer: function() {
-      var container = document.createElement('div');
-      container.setAttribute('class', 'funnel-bar-table-container');
-      container.append(this._getTable());
-      return container;
-    },
+    // _getTableContainer: function() {
+    //   var container = $('div');
+    //   container.attr('class', 'funnel-bar-table-container');
+    //   container.append(this._getTable());
+    //   return container;
+    // },
 
     _getChartTitle: function() {
-      var title = document.createElement('div');
-      title.setAttribute('class', 'funnel-bar-title');
+      var title = $('div');
+      title.attr('class', 'funnel-bar-title');
       title.append(this.title);
       return title;
     },
@@ -97,11 +97,11 @@
 
     _getChartContainer: function() {
       var line = d3.line().x(function(d){return d.x}).y(function(d){return d.y});
-      var container = document.createElement('div');
-      container.setAttribute('class', 'funnel-bar-chart-container');
+      var container = $('div');
+      container.attr('class', 'funnel-bar-chart-container');
       container.append(this._getChartTitle());
 
-      var c = d3.select(container)
+      var c = d3.select(container[0])
         .append("svg")
         .attr("width", '100%')
         .attr("height", 150);
@@ -121,53 +121,53 @@
       return container;
     },
 
-    _getTable: function() {
-      var table = document.createElement('table');
-      table.setAttribute('class', 'funnel-bar-table');
-      table.append(this._getTableHeader());
-      var _self = this;
-      this.series.forEach(function(dataItem, i) {
-        table.append(_self._getTableRow(dataItem, _self.colors[i]))
-      });
-      return table;
-    },
-
-    _getTableRow: function(dataRow, color) {
-      var tableRow = document.createElement('tr');
-      tableRow.setAttribute('class', 'funnel-bar-table-row');
-      var _self = this;
-
-      dataRow.forEach(function(dataItem, i) {
-        var cell = document.createElement('td');
-        if (i === 0) {
-          cell.append(_self._getColorBadge(color));
-        }
-        cell.append(dataItem.toLocaleString());
-        tableRow.append(cell)
-      });
-
-      return tableRow;
-    },
-
-    _getTableHeader: function() {
-      var header = document.createElement('thead');
-      header.setAttribute('class', 'funnel-bar-table-header');
-      var headerTr = document.createElement('tr');
-
-      this.headers.forEach(function(field) {
-        var cell = document.createElement('th');
-        cell.setAttribute('class', 'funnel-bar-table-header-cell');
-        cell.append(field);
-        headerTr.append(cell);
-      });
-      header.append(headerTr);
-      return header;
-    },
+    // _getTable: function() {
+    //   var table = $('table');
+    //   table.attr('class', 'funnel-bar-table');
+    //   table.append(this._getTableHeader());
+    //   var _self = this;
+    //   this.series.forEach(function(dataItem, i) {
+    //     table.append(_self._getTableRow(dataItem, _self.colors[i]))
+    //   });
+    //   return table;
+    // },
+    //
+    // _getTableRow: function(dataRow, color) {
+    //   var tableRow = $('tr');
+    //   tableRow.attr('class', 'funnel-bar-table-row');
+    //   var _self = this;
+    //
+    //   dataRow.forEach(function(dataItem, i) {
+    //     var cell = $('td');
+    //     if (i === 0) {
+    //       cell.append(_self._getColorBadge(color));
+    //     }
+    //     cell.append(dataItem.toLocaleString());
+    //     tableRow.append(cell)
+    //   });
+    //
+    //   return tableRow;
+    // },
+    //
+    // _getTableHeader: function() {
+    //   var header = $('thead');
+    //   header.attr('class', 'funnel-bar-table-header');
+    //   var headerTr = $('tr');
+    //
+    //   this.headers.forEach(function(field) {
+    //     var cell = $('th');
+    //     cell.attr('class', 'funnel-bar-table-header-cell');
+    //     cell.append(field);
+    //     headerTr.append(cell);
+    //   });
+    //   header.append(headerTr);
+    //   return header;
+    // },
 
     _getColorBadge: function(color) {
-      var colorBadge = document.createElement('div');
-      colorBadge.setAttribute('class', 'color-badge');
-      colorBadge.setAttribute('style', 'background-color:' + color+';');
+      var colorBadge = $('div');
+      colorBadge.attr('class', 'color-badge');
+      colorBadge.attr('style', 'background-color:' + color+';');
       return colorBadge;
     }
   });
